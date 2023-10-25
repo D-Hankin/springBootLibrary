6281e151d2756e0461f04a3cfe1bb4077c4525cf
David Hankin <d_hankin@outlook.com>
2023-10-25 12:16:18 +0200

    Spring files

---
+++ bootBiblioteket/src/main/java/bootBiblioteket/bootBiblioteket/BootBiblioteketApplication.java
@@ -0,0 +1,13 @@
+package bootBiblioteket.bootBiblioteket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBiblioteketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBiblioteketApplication.class, args);
+	}
+
+}
---
+++ bootBiblioteket/src/test/java/bootBiblioteket/bootBiblioteket/BootBiblioteketApplicationTests.java
@@ -0,0 +1,13 @@
+package bootBiblioteket.bootBiblioteket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBiblioteketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
